159435bf71e90a133242b3394982351c3aafd770
rupan <rupan@SSSOLVEM2800>
2024-07-01 12:17:31 +0530

    Add project files.

---
+++ arrays_Fun/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace arrayFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new int[5] ;
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 7;
+
+
+            var flags = new bool[3] ;
+            flags[0] = true;
+           
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+            
+                     
+
+        }
+    }
+}
